9caa267c9d3d7f19f2538a2b5ca7647c0e7e32b1
root <root@acer11.acer11>
2026-03-20 15:50:57 +0800

    changes in dbfarm.sql file

--- db/dbfarm.sql
+++ db/dbfarm.sql
@@ -1,70 +1,86 @@
-CREATE TABLE dbfarm2.tbuser (
-    id int(10) unsigned NOT NULL AUTO_INCREMENT,
-    UserName VARCHAR(45) NOT NULL, 
-    PassWord VARCHAR(255) NOT NULL, 
-    FullName VARCHAR(45) NOT NULL, 
-    role_id int (10) unsigned NOT NULL,
-    province_id int (10) unsigned NOT NULL,
-    cm_id int (10) unsigned NOT NULL,
+- Create database
+CREATE DATABASE IF NOT EXISTS dbfarm2;
+USE dbfarm2;
+
+-- =========================
+-- BASE TABLES (NO DEPENDENCIES)
+-- =========================
+
+CREATE TABLE tbrole (
+    role_id INT UNSIGNED AUTO_INCREMENT,
+    role_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (role_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE tbprovince (
+    province_id INT UNSIGNED AUTO_INCREMENT,
+    province_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (province_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE tbcitymuni (
+    cm_id INT UNSIGNED AUTO_INCREMENT,
+    cm_name VARCHAR(45) NOT NULL,
+    province_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (cm_id),
+    FOREIGN KEY (province_id) REFERENCES tbprovince(province_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE tbcrop (
+    crop_id INT UNSIGNED AUTO_INCREMENT,
+    crop_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (crop_id)
+) ENGINE=InnoDB;
+
+-- =========================
+-- DEPENDENT TABLES
+-- =========================
+
+CREATE TABLE tbuser (
+    id INT UNSIGNED AUTO_INCREMENT,
+    UserName VARCHAR(45) NOT NULL,
+    PassWord VARCHAR(255) NOT NULL,
+    FullName VARCHAR(45) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    province_id INT UNSIGNED NOT NULL,
+    cm_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES tbrole(role_id),
     FOREIGN KEY (province_id) REFERENCES tbprovince(province_id),
     FOREIGN KEY (cm_id) REFERENCES tbcitymuni(cm_id)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB;
 
-CREATE TABLE dbfarm2.tbrole (
-    role_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-    role_name VARCHAR (45) NOT NULL,
-    PRIMARY KEY (role_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-CREATE TABLE dbfarm2.audit_trail (
-    id int(11) unsigned NOT NULL AUTO_INCREMENT,
-    action VARCHAR(255) NOT NULL, 
-    user VARCHAR(45) NOT NULL, 
-    timestamp timestamp NOT NULL DEFAULT current_timestamp(), 
-    PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-CREATE TABLE dbfarm2.tbprovince (
-    province_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-    province_name VARCHAR (45) NOT NULL,
-    PRIMARY KEY (province_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-CREATE TABLE dbfarm2.tbcitymuni (
-    cm_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-    cm_name VARCHAR (45) NOT NULL,
-    province_id int(10) unsigned NOT NULL,
-    PRIMARY KEY (cm_id),
-    FOREIGN KEY (province_id) REFERENCES tbprovince(province_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-CREATE TABLE dbfarm2.tbfarm (
-	farm_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-	id int (10) unsigned NOT NULL,
-	province_id int (10) unsigned NOT NULL,
-	cm_id int (10) unsigned NOT NULL,
-	farm_name VARCHAR (45) NOT NULL,
-	PRIMARY KEY (farm_id),
+CREATE TABLE tbfarm (
+    farm_id INT UNSIGNED AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL,
+    province_id INT UNSIGNED NOT NULL,
+    cm_id INT UNSIGNED NOT NULL,
+    farm_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (farm_id),
     FOREIGN KEY (id) REFERENCES tbuser(id),
-	FOREIGN KEY (province_id) REFERENCES tbprovince(province_id),
-	FOREIGN KEY (cm_id) REFERENCES tbcitymuni(cm_id)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+    FOREIGN KEY (province_id) REFERENCES tbprovince(province_id),
+    FOREIGN KEY (cm_id) REFERENCES tbcitymuni(cm_id)
+) ENGINE=InnoDB;
 
-CREATE TABLE dbfarm2.tbcrop (
-    crop_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-    crop_name VARCHAR (45) NOT NULL,
-    PRIMARY KEY (crop_id)
-) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE tbfarmsupply (
+    fs_id INT UNSIGNED AUTO_INCREMENT,
+    farm_id INT UNSIGNED NOT NULL,
+    crop_id INT UNSIGNED NOT NULL,
+    fs_quantity VARCHAR(45) NOT NULL,
+    PRIMARY KEY (fs_id),
+    FOREIGN KEY (farm_id) REFERENCES tbfarm(farm_id),
+    FOREIGN KEY (crop_id) REFERENCES tbcrop(crop_id)
+) ENGINE=InnoDB;
 
-CREATE TABLE dbfarm2.tbfarmsupply (
-	fs_id int (10) unsigned NOT NULL AUTO_INCREMENT,
-	farm_id int (10) unsigned NOT NULL,
-	crop_id int (10) unsigned NOT NULL,
-	fs_quantity VARCHAR (45) NOT NULL,
-	PRIMARY KEY (fs_id),
-    	FOREIGN KEY (farm_id) REFERENCES tbfarm(farm_id),
-	FOREIGN KEY (crop_id) REFERENCES tbcrop(crop_id)
-) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+-- =========================
+-- INDEPENDENT TABLE
+-- =========================
+
+CREATE TABLE audit_trail (
+    id INT UNSIGNED AUTO_INCREMENT,
+    action VARCHAR(255) NOT NULL,
+    user VARCHAR(45) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
 

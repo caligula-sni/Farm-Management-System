9c154b0e7dc71da69a69354dfed6d59698597b9a
root <root@acer11.acer11>
2026-03-20 23:50:48 +0800

    Changes in Database Configs, Author: cali@debian

--- include/connect/dbcon.php
+++ include/connect/dbcon.php
@@ -3,7 +3,7 @@
     try {
 
         //connct to mysql
-        $pdoConnect = new PDO("mysql:host=localhost;dbname=dbfarm2", "root", "");
+        $pdoConnect = new PDO("mysql:host=localhost;dbname=dbfarm2", "user01", "2389");
         
         // set the PDO error mode to exception 
         $pdoConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

6e82d772ceeb0f19688f686b078f52369bc45bca
caligula-sni <janpascual143@gmail.com>
2026-04-28 08:00:16 +0000

    uname and pwrd changes

--- db/dbfarm.sql
+++ db/dbfarm.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS dbfarm2;
 -- Create database
 CREATE DATABASE IF NOT EXISTS dbfarm2;
 USE dbfarm2;
--- page/users/farmers/include/connect/dbcon.php
+++ page/users/farmers/include/connect/dbcon.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Update credentials below to match your MySQL setup
-    $pdoConnect = new PDO("mysql:host=localhost;dbname=dbfarm2", "root", "");
+    $pdoConnect = new PDO("mysql:host=localhost;dbname=dbfarm2", "admin01", "2389");
     $pdoConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exc) {
     die("Database connection failed: " . $exc->getMessage());
